e3afe53c7879db0dae48921634f9a9d5f1380177
Jamie Massey <JamieAlanMassey@gmail.com>
2017-05-02 14:55:22 +0100

    line-line intersection test

--- source/shapes/LineSegment.cpp
+++ source/shapes/LineSegment.cpp
@@ -8,7 +8,7 @@ namespace shape {
     LineSegment::LineSegment(std::array<float, 4> segment) :
         IShape(2)
     {
-    setLine(segment);
+        setLine(segment);
     }
 
     LineSegment::LineSegment(float x1, float y1, float x2, float y2) :
@@ -47,11 +47,31 @@ namespace shape {
 
     bool LineSegment::intersection(LineSegment& line)
     {
-        return false;
+        std::array<float, 4> targetLine = line.getLineSegment();
+        std::array<float, 2> targetA = { targetLine[0], targetLine[1] };
+        std::array<float, 2> targetB = { targetLine[2], targetLine[3] };
+
+        std::array<float, 2> lineA = { mLineSegment[0], mLineSegment[1] };
+        std::array<float, 2> lineB = { mLineSegment[2], mLineSegment[3] };
+
+        float d = ((targetB[1] - targetA[1]) * (lineB[0] - lineA[0])) -
+                  ((targetB[0] - targetA[0]) * (lineB[1] - lineA[1]));
+
+        if (d == 0.0f)
+            return false;
+
+        float ma = (((targetB[0] - targetA[0]) * (lineA[1] - targetA[1])) -
+                    ((targetB[1] - targetA[1]) * (lineA[0] - targetA[0]))) / d;
+
+        float mb = (((lineB[0] - lineA[0]) * (lineA[1] - targetA[1])) -
+                    ((lineB[1] - lineA[1]) * (lineA[0] - targetA[0]))) / d;
+
+        return ((ma < 0.0f && ma > 1.0f) && (mb < 0.0f && mb > 1.0f));
     }
 
     bool LineSegment::intersection(std::array<float, 2> point)
     {
+        // TODO: line-point intersection test
         return false;
     }
 
